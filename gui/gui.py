b1f971484a486dab1df1b2e56fc9af7d2eaab8c0
veaya <dhavidgogi@gmail.com>
2021-11-17 22:04:26 -0500

    bruh

---
+++ gui/gui.py
@@ -0,0 +1,10 @@
+#load the gooey
+import tkinter as tk
+
+
+
+#create the window
+window = tk.Tk()
+window.mainloop()
+showerButton = tk.Button(text = "Take a Shower", width=25,height=5,bg="grey",fg="white")
+#this hurts my head im going to sleep
